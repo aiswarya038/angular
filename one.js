89b150289ba38e13724f0dfbf382413198fcc39a
aiswarya038 <139538509+aiswarya038@users.noreply.github.com>
2024-01-02 00:09:56 -0800

    Add files via upload

---
+++ one.js
@@ -0,0 +1,35 @@
+var age = 21;
+console.log(age);
+var a = "hello";
+console.log(a);
+var b = true;
+console.log(b);
+var c = "mango";
+console.log(c);
+var d = 1000;
+console.log(d);
+var x = 10;
+var y = 20;
+if (x < y) {
+    var z = ("hello");
+    console.log(z);
+}
+console.log(x);
+console.log(y);
+console.log(z);
+var h = 10;
+var j = 20;
+if (h < j) {
+    var k = "good";
+    console.log(k);
+    console.log(h);
+}
+console.log(j);
+console.log(k);
+//function
+function hello(a, b) {
+    return a + b;
+}
+var x = hello(9, 9);
+console.log(x);
+// console.log(hello(8,9))
---
+++ one.ts
@@ -0,0 +1,43 @@
+var age:number=21;
+console.log(age)
+
+var a:string="hello";
+console.log(a)
+
+var b:boolean=true;
+console.log(b)
+
+var c:any="mango"
+console.log(c)
+
+var d:any=1000;
+console.log(d)
+
+var x=10;
+var y=20;
+if(x<y){
+    var z=("hello")
+    console.log(z)
+}
+console.log(x)
+console.log(y)
+console.log(z)
+
+let h=10;
+let j=20
+if(h<j){
+    var k="good"
+    console.log(k)
+
+console.log(h)}
+console.log(j)
+console.log(k)
+
+//function
+
+function hello(a:number,b:number){
+    return a+b
+}
+var x:number=hello(9,9)
+console.log(x```)
+// console.log(hello(8,9))
